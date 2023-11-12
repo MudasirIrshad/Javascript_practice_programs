9e00a80266ffe3fae06c6d75ec031240f6bdba9c
Mudasir Irshad <mudasirirshad47@gmail.com>
2023-11-13 02:33:42 +0500

    Hollow saquare Star Pattern

--- TrianglePattren.js
+++ TrianglePattren.js
@@ -70,6 +70,44 @@ Pattern:
 let str=''
 for (let i =1;i<=5;i++){
     str+='*'
-    
     console.log(str);
 }
+
+
+
+/*
+Pattern:
+------------------------------
+*****
+*   *
+*   *
+*   *
+*****
+-------------------------------
+*/
+console.log();
+function compLine(n){
+    let str=''
+    for(let i = 0; i<n;i++){
+        str+='*'
+    }
+    console.log(str);
+}
+function spaceBetween(n){
+    let str=''
+    str+='*'
+    for(let i =0;i<n-2;i++){
+        str+=' '
+    }
+    str+='*'
+    console.log(str);
+}
+function fullPattern(n){
+    compLine(n)
+    for(let i =0;i<n-2;i++){
+        spaceBetween(n)
+    }
+    compLine(n)
+}
+
+fullPattern(10)
